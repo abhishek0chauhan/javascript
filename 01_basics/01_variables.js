6afc2fb19c875e4c4b6553f1b8402cf88122b718
Abhishek Chauhan <48078873+abhishek0chauhan@users.noreply.github.com>
2023-11-29 16:54:26 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144457854
+let accountEmail = "abhi@gmail.com"
+var accountPassword = "password"
+accountCity = "Surat"
+let accountState
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS Code of this file as new version
+
+// alert("Hello")
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof "a");
