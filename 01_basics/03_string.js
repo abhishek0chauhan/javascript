0849d33fc41a35872588025451de3ec493240f24
Abhishek Chauhan <48078873+abhishek0chauhan@users.noreply.github.com>
2023-11-30 11:56:36 +0000

    Date

---
+++ 01_basics/03_string.js
@@ -0,0 +1,12 @@
+// let str = 'Abhishek'
+// let str1 = new String('Abhishek')
+
+// console.log(str[0]);
+// console.log(str1[2]);
+
+let date = new Date()
+
+
+
+console.log(date.toDateString());
+console.log(date.toString());
