aa4ea2729c46f7689436724d257a2e9f3f40b9bb
Abhishek Chauhan <48078873+abhishek0chauhan@users.noreply.github.com>
2023-12-04 13:47:11 +0000

    objects

---
+++ 01_basics/04_objects.js
@@ -0,0 +1,17 @@
+let course = {
+    name: "JS",
+    price: "222",
+    platfrom: "udemy"
+};
+
+
+// console.log(course.name);
+
+const {name, price, platfrom} = course;
+
+// const {name, price, platfrom: source} = course;
+
+console.log(name);
+console.log(price);
+console.log(platfrom);
+// console.log(source);
